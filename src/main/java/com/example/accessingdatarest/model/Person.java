cb2db579531db251cd0f0cab8a234efc9ea457b4
kschork <kevin.schork@she.net>
2020-11-04 23:38:54 +0100

    Initial commit

---
+++ src/main/java/com/example/accessingdatarest/model/Person.java
@@ -0,0 +1,35 @@
+package com.example.accessingdatarest.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private String id;
+	private String firstName;
+	private String lastName;
+	@CreationTimestamp
+	@JsonProperty(access = Access.READ_ONLY)
+	private LocalDateTime dateCreated;
+	@UpdateTimestamp
+	@JsonProperty(access = Access.READ_ONLY)
+	private LocalDateTime dateModified;
+
+}
---
+++ src/main/java/com/example/accessingdatarest/repository/PersonRepository.java
@@ -0,0 +1,31 @@
+package com.example.accessingdatarest.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.accessingdatarest.model.Person;
+
+@RepositoryRestResource
+public interface PersonRepository extends CrudRepository<Person, String> {
+	
+	List<Person> findAll(Sort sort);
+	
+	List<Person> findAllByFirstNameIgnoreCase(@Param("name") String name, Sort sort);
+
+	Page<Person> findByFirstNameIgnoreCase(@Param("name") String name, Pageable page);
+	
+	Page<Person> findByFirstNameStartsWithIgnoreCase(@Param("name") String name, Pageable page);
+	
+	List<Person> findAllByLastNameIgnoreCase(@Param("name") String name, Sort sort);
+
+	Page<Person> findByLastNameIgnoreCase(@Param("name") String name, Pageable page);
+	
+	Page<Person> findByLastNameStartsWithIgnoreCase(@Param("name") String name, Pageable page);
+
+}
